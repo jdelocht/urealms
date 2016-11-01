c4868336448cd0b5895391354d947e5a9d6d0c30
Joost de Locht <joost@echodes.nl>
2016-11-01 20:24:57 +0100

    creates a basic example character

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joost
+ * Date: 1-11-2016
+ * Time: 20:10
+ */
+
+echo 'Character Name: ' . '<br>'
+    . 'Gwynneth(Name) ' . 'Sunsword(LastName) ' . '<br>'
+    . 'Race: ' . '<br>'
+    . 'Noble Born(subrace) ' . 'Elf(race) ' . '<br>'
+    . 'Gender: ' . '<br>'
+    . 'Female ' . '<br>'
+    . 'Class: ' . '<br>'
+    . 'Sun Cleric ' . '<br>';
+
+
